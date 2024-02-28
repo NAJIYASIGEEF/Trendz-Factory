7e53de0d05e5b5b8bd506cff28cad4f0afa7a1ac
Najiya <najiyasigeef2000@gmail.com>
2024-02-28 16:33:28 +0530

    basket-list-items

--- store/views.py
+++ store/views.py
@@ -3,7 +3,7 @@ from django.views.generic import View,TemplateView
 from store.forms import RegistrationForm,LoginForm
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
-from store.models import Product
+from store.models import Product,BasketItem,Size
 
 # Create your views here.
 
@@ -58,3 +58,23 @@ class ProductDetailView(View):
     
 class HomeView(TemplateView):
     template_name="base.html"
+
+class AddToBasketView(View):
+    def post(self,request,*args,**kwargs):
+        size=request.POST.get("size")
+        size_obj=Size.objects.get(name=size)
+        qty=request.POST.get("qty")
+        id=kwargs.get("pk")
+        product_obj=Product.objects.get(id=id)
+        BasketItem.objects.create(
+            size_object=size_obj,
+            qty=qty,
+            product_object=product_obj,
+            basket_object=request.user.cart
+        )
+        return redirect("index")
+    
+class BasketItemListView(View):
+    def get(self,request,*args,**kwargs):
+        qs=request.user.cart.cartitem.filter(is_order_placed=False)
+        return render(request,"cart_list.html",{"data":qs})
